20b644688af55ce65db9f61717a3c32cfe839ca6
Gregory Pardini <gregorypardini.private@gmail.com>
2021-04-10 15:51:01 +0200

    Update Profilo.tsx

--- src/pages/Profilo.tsx
+++ src/pages/Profilo.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar , IonListHeader, IonItem, IonAvatar, IonList, IonCheckbox, IonLabel} from '@ionic/react';
+import { IonButton, IonButtons, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonTitle, IonToolbar , IonListHeader, IonItem, IonAvatar, IonList, IonCheckbox, IonLabel, useIonViewWillEnter} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import {heart, square, arrowForwardOutline} from 'ionicons/icons'
 
@@ -6,35 +6,66 @@ import HeaderLogo from '../components/HeaderLogo';
 import Notifiche from '../components/Notifiche';
 
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../components/use-reducer-context";
 import { State } from "ionicons/dist/types/stencil-public-runtime";
+import { db } from '../components/firebase';
 
+interface utente{
+  sesso: string,
+  eta: number,
+  quiz_categorieRischio: boolean,
+  quiz_stileDiVita:  boolean,
+  quiz_familiarita:boolean,
+  quiz_vaccini: boolean,
+  quiz_salutePersonale: boolean
+}
 const Profilo: React.FC = () => {
   const { state, dispatch } = useContext(AppContext);
-  console.log(state);
+  const [utente, setUtente] = useState({} as utente);
+
+  useIonViewWillEnter(() =>{
+    db.collection("users").doc(state.email).get().then((snapshot) =>{
+        let item = snapshot.data();
+        if(item != undefined){
+            let objLink = {
+                sesso: item.sesso,
+                eta: item.eta,
+                quiz_categorieRischio: item.quiz_categorieRischio,
+                quiz_stileDiVita:  item.quiz_stileDiVita,
+                quiz_familiarita: item.quiz_familiarita,
+                quiz_vaccini: item.quiz_vaccini,
+                quiz_salutePersonale: item.quiz_salutePersonale
+            }
+            setUtente(objLink);
+        }
+    })
+})
+
 
   return (
     <IonPage>
       <HeaderLogo />
       <IonContent>
-        Tab 1
+        
         <IonItem>
           <IonAvatar slot="start">
             <img src={state.img} />
           </IonAvatar>
-          {state.nome}
+          <h2>{state.nome}</h2>
+          <h3>{utente.sesso}, {utente.eta} anni</h3>
         </IonItem>
         <IonList>
           <IonListHeader>Sondaggi:</IonListHeader>
-      
-          <IonItem>
+
+         { utente.quiz_categorieRischio && ( <IonItem>
             <IonCheckbox checked={true} disabled={true}></IonCheckbox>      
             <IonLabel>Generale</IonLabel>
             <IonButton slot="end" href="/home/questionary">
               <IonIcon icon={arrowForwardOutline} slot="end" />
             </IonButton>
-          </IonItem>
+          </IonItem>)}
+          
         </IonList>
 
 
